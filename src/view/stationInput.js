fb1385e889fc13cb268c1552e843854b05c3afff
chanuuuuu <dn0208@gmail.com>
2021-05-13 22:58:40 +0900

    [refactor] 역 입력 뷰 리팩토링

--- src/view/stationInput.js
+++ src/view/stationInput.js
@@ -12,38 +12,46 @@ export default class StationInput {
     this.element = makeTag({
       "tag" : 'div',
     });
-    
-    const title = makeTag({
-      "tag" : 'div',
-      'text': '역 이름',
-      'style': 'font-weight: 700;'
-    })
 
     const innerDiv = makeTag({
       'tag': 'div',
       'style': 'margin-bottom: 10px;'
     })
 
+    this.insertTitleTag(this.element);
+    this.insertInputTag(innerDiv);
+    this.insertButtonTag(innerDiv);
+
+    this.element.appendChild(innerDiv)
+
+    return this.element;
+  }
+
+  insertTitleTag(parent) {
+    const title = makeTag({
+      "tag" : 'div',
+      'text': '역 이름',
+      'style': 'font-weight: 700;'
+    })
+    parent.appendChild(title);
+  }
+
+  insertInputTag(parent) {
     const input = makeTag({
       'tag' : 'input',
       'id' : STATION_INPUT_ID
     })
+    parent.appendChild(input);
+  }
 
+  insertButtonTag(parent) {
     const button = makeTag({
       'tag': 'button',
       'text' : '역 추가',
       'id' : STATION_INPUT_BUTTON_ID,
       'style': 'font-size: 15px;'
     })
-
-    innerDiv.appendChild(input);
-    innerDiv.appendChild(button);
-
-    this.element.appendChild(title);
-    this.element.appendChild(innerDiv)
-
-    return this.element;
+    parent.appendChild(button);
   }
-
 
 }
